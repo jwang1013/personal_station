6f6ad1afc4dd5aff2b38becb4595b145f876bd8a
jwang1013 <37950852+jwang1013@users.noreply.github.com>
2018-05-13 03:45:29 -0700

    finish setting up

--- personal_page/public/js/animation.js
+++ personal_page/public/js/animation.js
@@ -67,13 +67,34 @@ $(document).ready(function() {
                 multiply2++;
             });
         }
-        if ($elemTL.offset().top <= $window.scrollTop() + $window.height() / 2) {
-            window.requestAnimationFrame(function() {
-                document.querySelector('.timeline').classList.add('timeline--show');
-            });
-        }
     });
+    /* experience timeline */
+   'use strict';
+     
+   var items = document.querySelectorAll(".timeline li");
 
+   // check if an element is in viewport
+   function isElementInViewport(el) {
+       var rect = el.getBoundingClientRect();
+       return (
+           rect.top >= 0 &&
+           rect.left >= 0 &&
+           rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+           rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+       );
+   }
+
+   function callbackFunc() {
+       for (var i = 0; i < items.length; i++) {
+           if (isElementInViewport(items[i])) {
+               items[i].classList.add("in-view");
+           }
+       }
+   }
+   // listen for events
+   window.addEventListener("load", callbackFunc);
+   window.addEventListener("resize", callbackFunc);
+   window.addEventListener("scroll", callbackFunc);
     
     
 });
